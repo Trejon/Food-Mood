43470b58dec01b5da3dcb7b8033b1fcf05218de7
Trejon Stallsworth <TrejonStallsworth@gmail.com>
2020-04-29 15:35:10 -0600

    added .merge to my user params to fix the issue of whitelisting the password

--- food-mood-backend/app/controllers/api/v1/users_controller.rb
+++ food-mood-backend/app/controllers/api/v1/users_controller.rb
@@ -15,10 +15,7 @@ class Api::V1::UsersController < ApplicationController
 
   # POST /users
   def create
-    # @user = User.create(name: params[:name], email: params[:email], password: params[:password])
     @user = User.create(user_params) 
-    @user.password = params[:password]
-    byebug
     if @user.save
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created
@@ -52,6 +49,6 @@ class Api::V1::UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_digest)
+      params.require(:user).permit(:name, :email).merge(password: params[:password])
     end
 end
