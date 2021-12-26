2d8e8531b1e0d598e9caa620f3870736faaf3a24
Trejon <Trejonstallsworth@gmail.com>
2021-12-26 16:47:32 -0700

    fixed home page

--- food-mood-frontend/src/components/Home.js
+++ food-mood-frontend/src/components/Home.js
@@ -7,109 +7,112 @@ import { Button, Divider, Form, Grid, Segment } from 'semantic-ui-react';
 import AuthForm from './user/Login';
 import SignUp from './user/SignUp';
 import '../App.css';
-
-// const NoAccountHomePage = () => (
-//   <Segment placeholder>
-//     <Grid columns={2} relaxed="very" stackable>
-//       <Grid.Column verticalAlign="middle">
-//         <Button
-//           content="Sign up"
-//           icon="signup"
-//           size="big"
-//           onClick={() => history.push('/signup')}
-//         />
-//       </Grid.Column>
-
-//       <Grid.Column>
-//         <AuthForm />
-//       </Grid.Column>
-//     </Grid>
-
-//     <Divider vertical></Divider>
-//   </Segment>
-// );
-
-class AccountHomePage extends React.Component {
+class Home extends React.Component {
   state = {
     signup: false,
   };
 
+  handleButtonClick = () => {
+    this.setState((prevState) => ({
+      signup: !prevState.signup,
+    }));
+  };
+
   render() {
-    if (this.state.signUp) {
+    if (!this.props.currentUser && this.state.signup) {
       return (
-        <Segment placeholder>
-          <Grid columns={2} relaxed="very" stackable>
-            <Grid.Column className="homeGrid" verticalAlign="middle">
-              <Button
-                content="Sign Up"
-                icon="login"
-                size="big"
-                onClick={() =>
-                  this.setState((prevState) => ({
-                    signup: !prevState.signup,
-                  }))
-                }
-              />
-            </Grid.Column>
+        <div>
+          <Segment placeholder>
+            <Grid columns={2} relaxed="very" stackable>
+              <Grid.Column className="homeGrid" verticalAlign="middle">
+                <Button
+                  content="Login"
+                  icon="login"
+                  size="massive"
+                  onClick={this.handleButtonClick}
+                >
+                  Login
+                </Button>
+              </Grid.Column>
 
-            <Grid.Column>
-              <AuthForm />
-            </Grid.Column>
-          </Grid>
+              <Grid.Column>
+                <SignUp />
+              </Grid.Column>
+            </Grid>
+            <Divider vertical></Divider>
+          </Segment>
+        </div>
+      );
+    } else if (!this.props.currentUser && !this.state.signup) {
+      return (
+        <div>
+          <Segment placeholder>
+            <Grid columns={2} relaxed="very" stackable>
+              <Grid.Column className="homeGrid" verticalAlign="middle">
+                <Button
+                  content="Sign Up"
+                  icon="signup"
+                  size="massive"
+                  onClick={this.handleButtonClick}
+                >
+                  Sign Up
+                </Button>
+              </Grid.Column>
 
-          <Divider vertical></Divider>
-        </Segment>
+              <Grid.Column>
+                <AuthForm
+                  content="Log Up"
+                  icon="login"
+                  size="massive"
+                  onClick={() => this.handleButtonClick}
+                >
+                  Log In
+                </AuthForm>
+              </Grid.Column>
+            </Grid>
+            <Divider vertical></Divider>
+          </Segment>
+        </div>
       );
     } else {
-      return (
-        <Segment placeholder>
-          <Grid columns={2} relaxed="very" stackable>
-            <Grid.Column verticalAlign="middle" className="homeGrid">
-              <Button
-                content="Log In"
-                icon="login"
-                size="big"
-                onClick={() =>
-                  this.setState((prevState) => ({
-                    signup: !prevState.signup,
-                  }))
-                }
-              />
-            </Grid.Column>
-
-            <Grid.Column>
-              <SignUp />
-            </Grid.Column>
-          </Grid>
-
-          <Divider vertical></Divider>
-        </Segment>
-      );
+      return <MyLists />;
     }
-  }
-}
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        {' '}
-        {!this.props.currentUser ? (
-          <div className="ui message">
-            <div className="ui center aligned header">
-              <br />
-              <h3>Enable your location to enjoy all of the features. </h3>{' '}
-              <h6>
-                {' '}
-                <AccountHomePage />
-                Note: Google Map will have a filter due to the free API.{' '}
-              </h6>{' '}
-            </div>{' '}
-          </div>
-        ) : (
-          <MyLists />
-        )}{' '}
-      </div>
-    );
+    //     <div>
+    //       {' '}
+    //       <div className="ui message">
+    //         <div className="ui center aligned header">
+    //           <br />
+    //           <h3>Enable your location to enjoy all of the features. </h3>{' '}
+    //           <h6>
+    //             {' '}
+    //             {/* <AccountHomePage /> */}
+    //             <SignUpPage />
+    //             Note: Google Map will have a filter due to the free API.{' '}
+    //           </h6>{' '}
+    //         </div>{' '}
+    //       </div>
+    //     </div>
+    //   );
+    // } else if (!this.props.currentUser && !this.state.signup) {
+    //   return (
+    //     <div>
+    //       {' '}
+    //       <div className="ui message">
+    //         <div className="ui center aligned header">
+    //           <br />
+    //           <h3>Enable your location to enjoy all of the features. </h3>{' '}
+    //           <h6>
+    //             {' '}
+    //             {/* <AccountHomePage /> */}
+    //             <LoginPage />
+    //             Note: Google Map will have a filter due to the free API.{' '}
+    //           </h6>{' '}
+    //         </div>{' '}
+    //       </div>
+    //     </div>
+    //   );
+    // } else {
+    //   return <MyLists />;
   }
 }
 
