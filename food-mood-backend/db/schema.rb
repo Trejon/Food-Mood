174fbaf385bb9b36a3da8d0cafcf08d645581d93
Trejon <Trejonstallsworth@gmail.com>
2022-01-02 11:45:56 -0700

    update

--- food-mood-backend/db/schema.rb
+++ food-mood-backend/db/schema.rb
@@ -11,39 +11,40 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_19_181426) do
-  create_table 'lists', force: :cascade do |t|
-    t.string 'name'
-    t.text 'description'
-    t.integer 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_lists_on_user_id'
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
-  create_table 'meals', force: :cascade do |t|
-    t.string 'name'
-    t.string 'meal_type', default: 'lunch'
-    t.string 'kind'
-    t.string 'description'
-    t.string 'url'
-    t.date 'meal_date'
-    t.integer 'user_id', null: false
-    t.integer 'list_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['list_id'], name: 'index_meals_on_list_id'
-    t.index ['user_id'], name: 'index_meals_on_user_id'
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
+    t.string "meal_type", default: "lunch"
+    t.string "kind"
+    t.string "description"
+    t.string "url"
+    t.date "meal_date"
+    t.integer "user_id", null: false
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_meals_on_list_id"
+    t.index ["user_id"], name: "index_meals_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key 'lists', 'users'
-  add_foreign_key 'meals', 'lists'
-  add_foreign_key 'meals', 'users'
+  add_foreign_key "lists", "users"
+  add_foreign_key "meals", "lists"
+  add_foreign_key "meals", "users"
 end
