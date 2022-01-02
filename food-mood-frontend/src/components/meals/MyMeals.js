5fde521c2efcc7934c9ebaec041092e434f82ceb
Trejon <Trejonstallsworth@gmail.com>
2022-01-01 23:05:06 -0700

    meals

--- food-mood-frontend/src/components/meals/MyMeals.js
+++ food-mood-frontend/src/components/meals/MyMeals.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Icon, List } from 'semantic-ui-react';
+import { Icon, List, Grid } from 'semantic-ui-react';
 
 const MyMeals = (props) => {
   const mealCards =
     props.meals.length > 0 ? (
       props.meals.map((m) => (
         <List.Item className="item" key={m.id}>
-          <List.Icon size="massive" verticalAlign="middle">
+          {/* <List.Icon size="massive" verticalAlign="middle">
             <img src="https://img.icons8.com/pastel-glyph/64/000000/meal--v1.png" />
-          </List.Icon>
+          </List.Icon> */}
           <List.Content>
-            <List.Header as="a">
+            <List.Header as="div">
               <h5>
                 <Link to={`/meals/${m.id}`}> {m.attributes.name} </Link>
               </h5>
             </List.Header>
-            <List.Description> {m.attributes.description} </List.Description>{' '}
+            <List.Description as="div">
+              {m.attributes.description}
+            </List.Description>
           </List.Content>
         </List.Item>
       ))
@@ -27,14 +29,17 @@ const MyMeals = (props) => {
 
   return (
     <div className="list-content ">
-      <h3>These are your meals:</h3>
-
       <List divided relaxed animated celled size="massive">
-        {mealCards}{' '}
+        {mealCards}
       </List>
-      <Link className="ui button primary" to="/meals/new">
-        Create A New Meal
-      </Link>
+      <Grid columns={2} centered>
+        <Link className="ui button secondary meals" to="/restaurants">
+          Search Restaurants
+        </Link>
+        <Link className="ui button primary meals" to="/recipes">
+          Search Recipes
+        </Link>
+      </Grid>
     </div>
   );
 };
