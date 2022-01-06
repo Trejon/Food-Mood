149610955829efdb251272d13fb4924d1659683a
Trejon <Trejonstallsworth@gmail.com>
2022-01-05 18:18:40 -0700

    update list form

--- food-mood-frontend/src/components/lists/ListForm.js
+++ food-mood-frontend/src/components/lists/ListForm.js
@@ -1,26 +1,31 @@
-import React from 'react'; 
+import React from 'react';
 import { updateListForm } from '../../actions/listForm';
 import { connect } from 'react-redux';
 
-const ListForm = ({ formData, updateListForm, userId, list, handleSubmit, editMode }) => {
-  const { name, description } = formData
-  
-  const handleChange = event => {
-    const { name, value} = event.target
-    updateListForm(name, value)
-  } 
-  
+const ListForm = ({ formData, updateListForm, handleSubmit, editMode }) => {
+  const { name, description } = formData;
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    updateListForm(name, value);
+  };
+
   return (
-    <form className="ui form list-form" onSubmit={event => handleSubmit(event, formData)}> 
-      <input 
+    <form
+      className="ui form list-form"
+      onSubmit={(event) => handleSubmit(event, formData)}
+    >
+      <input
         name="name"
         onChange={handleChange}
         value={name}
         placeholder="Name"
         autoComplete="off"
         className="list-input"
-      /><br /><br/>
-      <input 
+      />
+      <br />
+      <br />
+      <input
         name="description"
         onChange={handleChange}
         value={description}
@@ -28,17 +33,20 @@ const ListForm = ({ formData, updateListForm, userId, list, handleSubmit, editMo
         autoComplete="off"
         className="list-input"
       />
-      <button className="ui primary button list-btn" type="submit">{editMode ? "Update List" : "Create List"}</button>
+      <button className="ui primary button list-btn" type="submit">
+        {' '}
+        {editMode ? 'Update List' : 'Create List'}{' '}
+      </button>{' '}
     </form>
-  )
-}
+  );
+};
 
-const mapStateToProps = state => {
-  const userId =  state.currentUser ? state.currentUser.currentUser.data.id : ""
+const mapStateToProps = (state) => {
+  const userId = state.currentUser ? state.currentUser.currentUser.data.id : '';
   return {
-    formData: state.ListForm, 
-    userId
-  }
-}
+    formData: state.ListForm,
+    userId,
+  };
+};
 
 export default connect(mapStateToProps, { updateListForm })(ListForm);
