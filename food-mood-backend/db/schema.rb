c3b66b24ed3da6f7f722ce8251b646a97e445281
Trejon <trejonstallsworth@gmail.com>
2023-01-25 20:07:25 -0700

    starting point

--- food-mood-backend/db/schema.rb
+++ food-mood-backend/db/schema.rb
@@ -2,22 +2,21 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_19_181426) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_04_19_181426) do
   create_table "lists", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
@@ -30,8 +29,8 @@ ActiveRecord::Schema.define(version: 2020_04_19_181426) do
     t.date "meal_date"
     t.integer "user_id", null: false
     t.integer "list_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_meals_on_list_id"
     t.index ["user_id"], name: "index_meals_on_user_id"
   end
@@ -40,8 +39,8 @@ ActiveRecord::Schema.define(version: 2020_04_19_181426) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "lists", "users"
